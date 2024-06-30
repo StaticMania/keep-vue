494c74e249a8bb553d3fbfc451d8749799f45c4c
shuvo-me <shuvo.js.dev@gmail.com>
2024-06-30 17:22:51 +0600

    this commit shows how can you add doc file with layout and add vue component in md file.

---
+++ pages/docs/components-md/[...slug].vue
@@ -0,0 +1,14 @@
+<template>
+    <docs-content-layout title="test tile" description="Hello description">
+        <ContentDoc />
+    </docs-content-layout>
+</template>
+
+<script setup lang="ts">
+
+
+definePageMeta({
+    layout: 'docs'
+})
+
+</script>
