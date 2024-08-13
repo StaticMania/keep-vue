57f6b723e8462f15733f9ce4c1ef53d935325c85
moshiur01 <mr.moshiur314@gmail.com>
2024-08-13 12:25:14 +0600

    Update: change the navbar logo src location

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,16 +1,17 @@
+<script setup></script>
+
 <template>
   <header class="fixed top-0 z-50 w-full border-b border-metal-100 bg-white">
     <div class="relative mx-auto max-w-7xl px-6 2xl:px-0">
       <nav class="flex items-center justify-between py-4">
         <NuxtLink to="/">
-          <NuxtImg src="/keep.svg" width="130" height="53" />
+          <NuxtImg src="/images/keep.svg" width="130" height="53" />
         </NuxtLink>
 
         <div>
           <NuxtLink
             to="/docs/getting-started/introduction"
-            class="rounded-lg bg-metal-900 px-4 py-2.5 text-body-4 font-normal capitalize text-white transition-all duration-300 hover:bg-metal-800"
-          >
+            class="rounded-lg bg-metal-900 px-4 py-2.5 text-body-4 font-normal capitalize text-white transition-all duration-300 hover:bg-metal-800">
             Get Started
           </NuxtLink>
         </div>
@@ -18,6 +19,3 @@
     </div>
   </header>
 </template>
-<script>
-export default {};
-</script>
