f798bc392c171f9beebfac29203a4a5d38b1ee34
moshiur01 <mr.moshiur314@gmail.com>
2024-08-13 19:49:55 +0600

    Update: Remove old Badge component varient and fix VarientOfBadges class style

--- pages/docs/components/badge/Variants/VariantOfBadge.vue
+++ pages/docs/components/badge/Variants/VariantOfBadge.vue
@@ -3,22 +3,22 @@ import { Badge } from "~/src/components/Badge";
 </script>
 
 <template>
-  <div className="space-y-4 p-2">
-    <div className="space-x-3">
+  <div class="space-y-4 p-2">
+    <div class="space-x-3">
       <Badge variant="base" color="primary">Primary</Badge>
       <Badge variant="base" color="secondary">Secondary</Badge>
       <Badge variant="base" color="success">Success</Badge>
       <Badge variant="base" color="warning">Warning</Badge>
       <Badge variant="base" color="error">Error</Badge>
     </div>
-    <div className="space-x-3">
+    <div class="space-x-3">
       <Badge variant="border" color="primary">Primary</Badge>
       <Badge variant="border" color="secondary">Secondary</Badge>
       <Badge variant="border" color="success">Success</Badge>
       <Badge variant="border" color="warning">Warning</Badge>
       <Badge variant="border" color="error">Error</Badge>
     </div>
-    <div className="space-x-3">
+    <div class="space-x-3">
       <Badge variant="background" color="primary">Primary</Badge>
       <Badge variant="background" color="secondary">Secondary</Badge>
       <Badge variant="background" color="success">Success</Badge>
