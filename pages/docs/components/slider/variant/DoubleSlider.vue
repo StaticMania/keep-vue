0146ce85d3127eca84ea5767df1ce9f34d0e1b61
moshiur01 <mr.moshiur314@gmail.com>
2024-09-06 13:08:03 +0600

    Fixed: change className to class in DoubleSlider Variant.

--- pages/docs/components/slider/variant/DoubleSlider.vue
+++ pages/docs/components/slider/variant/DoubleSlider.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div className="p-6">
-    <Slider :min="0" :max="100" :defaultValue="[33, 75]" />
+  <div class="p-6">
+    <Slider :min="0" :max="100" :default-value="[33, 75]" />
   </div>
 </template>
