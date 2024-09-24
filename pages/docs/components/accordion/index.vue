441bfa0ddf8451b419a429d6344dd8206459bdfe
moshiur01 <mr.moshiur314@gmail.com>
2024-09-24 13:36:08 +0600

    Fixed: Edit page component pageLink prop issue.

--- pages/docs/components/accordion/index.vue
+++ pages/docs/components/accordion/index.vue
@@ -20,7 +20,7 @@ if (!page)
     :description="metadata.description">
     <ContentRenderer id="mainContent" :value="page" />
     <EditPage
-      :page-link="page?.path"
+      :page-link="page?._path"
       :next-page-link="next?._path"
       :next-page-name="next?.title" />
   </DocsContentLayout>
