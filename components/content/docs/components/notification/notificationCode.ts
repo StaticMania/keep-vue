ca6c707a3ea52656be723d1acd559502b14e61f2
moshiur01 <mr.moshiur314@gmail.com>
2024-10-01 17:56:27 +0600

    Added: Notificatoin page doc.

--- components/content/docs/components/notification/notificationCode.ts
+++ components/content/docs/components/notification/notificationCode.ts
@@ -1,4 +1,5 @@
-<script lang="ts" setup>
+const DefaultNotificationCode = {
+  "NotificationComponent.vue": `<script setup>
 import {
   Button,
   Notification,
@@ -8,11 +9,46 @@ import {
   NotificationDescription,
   NotificationFooter,
   NotificationTitle,
-} from "~/src";
+} from "keep-vue";
 </script>
 
 <template>
-  <div class="flex items-center justify-center gap-5 px-5 py-3">
+  <Notification>
+    <NotificationAction as-child>
+      <Button>Open Notification</Button>
+    </NotificationAction>
+    <NotificationContent class="sm:max-w-[425px]">
+      <NotificationTitle>Update Notification Status</NotificationTitle>
+      <NotificationDescription>
+        Your document has unsaved changes. Discard or save them as a new page to
+        continue.
+      </NotificationDescription>
+
+      <NotificationFooter>
+        <NotificationClose as-child>
+          <Button>Accept</Button>
+        </NotificationClose>
+      </NotificationFooter>
+    </NotificationContent>
+  </Notification>
+</template>`,
+};
+
+const NotificationPositionCode = {
+  "NotificationComponent.vue": `<script setup>
+import {
+  Button,
+  Notification,
+  NotificationAction,
+  NotificationClose,
+  NotificationContent,
+  NotificationDescription,
+  NotificationFooter,
+  NotificationTitle,
+} from "keep-vue";
+</script>
+
+<template>
     <Notification position="bottomLeft">
       <NotificationAction as-child>
         <Button>Bottom Left</Button>
@@ -113,5 +149,58 @@ import {
         </NotificationFooter>
       </NotificationContent>
     </Notification>
-  </div>
-</template>
+</template>`,
+};
+
+const NotificationWithCardCode = {
+  "NotificationComponent.vue": `<script setup>
+import {
+  Button,
+  Notification,
+  NotificationAction,
+  NotificationClose,
+  NotificationContent,
+  NotificationDescription,
+  NotificationFooter,
+  NotificationTitle,
+} from "keep-vue";
+</script>
+
+<template>
+    <Notification>
+      <NotificationAction as-child>
+        <Button>Open Notification</Button>
+      </NotificationAction>
+      <NotificationContent
+        class="mb-2 max-w-xs space-y-4 overflow-hidden rounded-xl">
+        <NuxtImg
+          src="https://via.placeholder.com/600x400"
+          alt="image"
+          class="rounded-lg"
+          :width="600"
+          :height="400" />
+        <div>
+          <NotificationTitle>Can we store cookies?</NotificationTitle>
+          <NotificationDescription>
+            Default message - Lorem Ipsum is simply dummy text of the printing
+            and typesetting industry.
+          </NotificationDescription>
+        </div>
+        <NotificationFooter>
+          <NotificationClose as-child>
+            <Button>Accept</Button>
+          </NotificationClose>
+          <NotificationClose as-child>
+            <Button variant="outline">Review</Button>
+          </NotificationClose>
+        </NotificationFooter>
+      </NotificationContent>
+    </Notification>
+</template>`,
+};
+
+export {
+  DefaultNotificationCode,
+  NotificationPositionCode,
+  NotificationWithCardCode,
+};
---
+++ content/docs/components/notification.md
@@ -0,0 +1,33 @@
+---
+title: "Notification"
+description: "Notification component in the Keep React allows you to display informative messages or alerts to users. With various styles and options for positioning, you can effectively communicate important updates or messages in a visually appealing manner."
+---
+
+## Default Notification
+
+Display informative messages or alerts to users with the Default Notification component in Keep React.
+::DefaultNotification
+::
+
+## Notification With Card
+
+Enhance the Notification component with a card layout for a more visually appealing display.
+::NotificationWithCard
+::
+
+## Notification Position
+
+Position notifications according to your preference with available options: `"bottomRight" , "topLeft", "topCenter", "topRight", "bottomLeft"`. Default position is `"bottomRight"`. you can use the value in the `position` property.
+
+::NotificationPosition
+::
+
+## API Reference
+
+Explore the available props for the Notification component.
+::NotificationApi
+::
+
+## Reference
+
+To learn more about the Notification component, please see the documentation of [Redix Vue Dialog](https://www.radix-vue.com/components/dialog.html#dialog).
