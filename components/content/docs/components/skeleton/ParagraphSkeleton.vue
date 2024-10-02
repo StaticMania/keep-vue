ccf5bcca98bf5b1e1d1561cf452c1d0a7f5747e6
moshiur01 <mr.moshiur314@gmail.com>
2024-10-02 16:32:06 +0600

    Added: Skeleton doc.

---
+++ components/content/docs/components/skeleton/ParagraphSkeleton.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import CodeHighlightWithPreview from "~/components/content/CodeHighlightWithPreview.vue";
+import { Skeleton, SkeletonLine } from "~/src";
+import { ParagraphSkeletonCode } from "./skeletonCode";
+</script>
+
+<template>
+  <CodeHighlightWithPreview :code="ParagraphSkeletonCode">
+    <Skeleton class="max-w-xl space-y-2.5">
+      <SkeletonLine class="h-4 w-11/12" />
+      <SkeletonLine class="h-4 w-9/12" />
+      <SkeletonLine class="h-4 w-10/12" />
+      <SkeletonLine class="h-4 w-7/12" />
+    </Skeleton>
+  </CodeHighlightWithPreview>
+</template>
