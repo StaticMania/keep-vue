605ac9c85d18f5b8d17fec271e7e54ccf645821d
moshiur01 <mr.moshiur314@gmail.com>
2024-10-03 18:24:56 +0600

    Added: Flex doc of layout section.

---
+++ components/content/docs/layout/flex/variant/FlexLayoutThree.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import CodeHighlightWithPreview from "~/components/content/CodeHighlightWithPreview.vue";
+import { FlexLayoutThreeCode } from "../flexCode";
+</script>
+
+<template>
+  <CodeHighlightWithPreview :code="FlexLayoutThreeCode">
+    <div class="flex flex-col gap-5">
+      <div
+        class="basis-full rounded-xl border border-dashed bg-metal-25 p-10 dark:border-metal-800 dark:bg-metal-900"></div>
+      <div class="flex flex-wrap gap-5 lg:flex-nowrap">
+        <div
+          class="basis-full rounded-xl border border-dashed bg-metal-25 p-10 lg:basis-1/4 dark:border-metal-800 dark:bg-metal-900"></div>
+        <div
+          class="basis-full rounded-xl border border-dashed bg-metal-25 p-10 lg:basis-1/4 dark:border-metal-800 dark:bg-metal-900"></div>
+        <div
+          class="basis-full rounded-xl border border-dashed bg-metal-25 p-10 lg:basis-1/4 dark:border-metal-800 dark:bg-metal-900"></div>
+        <div
+          class="basis-full rounded-xl border border-dashed bg-metal-25 p-10 lg:basis-1/4 dark:border-metal-800 dark:bg-metal-900"></div>
+      </div>
+    </div>
+  </CodeHighlightWithPreview>
+</template>
