0f99e2360362ba469b8c35ae8bf5407a3b296c3f
moshiur01 <mr.moshiur314@gmail.com>
2024-10-03 18:47:14 +0600

    Added: Grid doc of layout section.

---
+++ components/content/docs/layout/grid/variant/GridLayoutTwo.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import { GridLayoutTwoCode } from "../girdLayoutCode";
+</script>
+
+<template>
+  <CodeHighlightWithPreview :code="GridLayoutTwoCode">
+    <div class="grid grid-cols-12 gap-5">
+      <div
+        class="col-span-12 rounded-xl border border-dashed bg-metal-25 p-10 lg:col-span-8 dark:border-metal-800 dark:bg-metal-900"></div>
+      <div
+        class="col-span-12 rounded-xl border border-dashed bg-metal-25 p-10 lg:col-span-4 dark:border-metal-800 dark:bg-metal-900"></div>
+      <div
+        class="col-span-4 rounded-xl border border-dashed bg-metal-25 p-10 lg:col-span-4 dark:border-metal-800 dark:bg-metal-900"></div>
+      <div
+        class="col-span-4 rounded-xl border border-dashed bg-metal-25 p-10 lg:col-span-4 dark:border-metal-800 dark:bg-metal-900"></div>
+      <div
+        class="col-span-4 rounded-xl border border-dashed bg-metal-25 p-10 lg:col-span-4 dark:border-metal-800 dark:bg-metal-900"></div>
+      <div
+        class="order-1 col-span-6 rounded-xl border border-dashed bg-metal-25 p-10 lg:order-1 lg:col-span-3 dark:border-metal-800 dark:bg-metal-900"></div>
+      <div
+        class="order-3 col-span-12 rounded-xl border border-dashed bg-metal-25 p-10 lg:order-2 lg:col-span-6 dark:border-metal-800 dark:bg-metal-900"></div>
+      <div
+        class="order-2 col-span-6 rounded-xl border border-dashed bg-metal-25 p-10 lg:order-3 lg:col-span-3 dark:border-metal-800 dark:bg-metal-900"></div>
+    </div>
+  </CodeHighlightWithPreview>
+</template>
---
+++ content/docs/layout/grid.md
@@ -0,0 +1,28 @@
+---
+title: "Grid"
+description: "A grid layout is a design technique that organizes content into rows and columns, providing a structured and flexible way to arrange elements on a webpage."
+---
+
+## Layout One
+
+::GridLayoutOne
+::
+
+## Layout Two
+
+::GridLayoutTwo
+::
+
+## Layout Three
+
+::GridLayoutThree
+::
+
+## Layout Four
+
+::GridLayoutFour
+::
+
+## Reference
+
+If you want to know more about grid layouts then you can read the documentation of tailwind css [grid](https://tailwindcss.com/docs/grid-template-columns) sections.
