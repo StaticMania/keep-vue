95a136bbb09fc3b5e1459bc11819f3d90f0fc54b
moshiur01 <mr.moshiur314@gmail.com>
2024-10-03 13:31:17 +0600

    Added: Upload component doc.

---
+++ components/content/docs/components/upload/UploadApi.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import ComponentApiTable from "~/components/content/ComponentApiTable.vue";
+
+const uploadApiData = [
+  {
+    id: 3,
+    propsName: "horizontal",
+    propsType: "boolean",
+    propsDescription: "Upload show horizontally or vertically",
+    default: "false",
+  },
+  {
+    id: 4,
+    propsName: "options",
+    propsType: "DropzoneOptions",
+    propsDescription: "Option is object for controlling the upload",
+    default: "DropzoneOptions",
+  },
+];
+</script>
+
+<template>
+  <ComponentApiTable :data="uploadApiData" />
+</template>
