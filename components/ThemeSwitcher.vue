6d4d648d282cfbc1ecb7f83d1e9c5f0e868dc50b
moshiur01 <mr.moshiur314@gmail.com>
2024-10-13 15:54:10 +0600

    Update: ThemeSwither style updated.

--- components/ThemeSwitcher.vue
+++ components/ThemeSwitcher.vue
@@ -11,7 +11,7 @@ const toggleTheme = () => {
 <template>
   <ClientOnly>
     <Button
-      class="rounded-lg bg-primary-25 p-2.5 transition duration-300 hover:bg-primary-50 dark:bg-metal-600 dark:hover:bg-metal-700"
+      class="rounded-lg border border-metal-100 bg-white p-2.5 transition-all duration-300 hover:bg-metal-25 dark:border-metal-800 dark:bg-metal-900 dark:hover:border-metal-600 dark:hover:bg-metal-900"
       @click="() => toggleTheme()">
       <PhosphorIconMoon v-if="colorMode.preference === 'dark'" :size="21" />
       <PhosphorIconSunDim v-else color="black" :size="21" />
