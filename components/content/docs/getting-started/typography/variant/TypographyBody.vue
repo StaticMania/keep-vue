e67a06bdf5e67dead67462c0326bb3f94ffb352a
moshiur01 <mr.moshiur314@gmail.com>
2024-10-31 20:45:52 +0600

    Fixed: Typography dark mode show issue solved.

--- components/content/docs/getting-started/typography/variant/TypographyBody.vue
+++ components/content/docs/getting-started/typography/variant/TypographyBody.vue
@@ -6,11 +6,21 @@ import { TypographyBodyCode } from "../typographyCode";
 <template>
   <CodeHighlightWithPreview :code="TypographyBodyCode" :active-tab="1">
     <div class="space-y-4">
-      <p class="text-body-1">The quick brown fox jumps over the lazy dog</p>
-      <p class="text-body-2">The quick brown fox jumps over the lazy dog</p>
-      <p class="text-body-3">The quick brown fox jumps over the lazy dog</p>
-      <p class="text-body-4">The quick brown fox jumps over the lazy dog</p>
-      <p class="text-body-5">The quick brown fox jumps over the lazy dog</p>
+      <p class="text-body-1 dark:text-white">
+        The quick brown fox jumps over the lazy dog
+      </p>
+      <p class="text-body-2 dark:text-white">
+        The quick brown fox jumps over the lazy dog
+      </p>
+      <p class="text-body-3 dark:text-white">
+        The quick brown fox jumps over the lazy dog
+      </p>
+      <p class="text-body-4 dark:text-white">
+        The quick brown fox jumps over the lazy dog
+      </p>
+      <p class="text-body-5 dark:text-white">
+        The quick brown fox jumps over the lazy dog
+      </p>
     </div>
   </CodeHighlightWithPreview>
 </template>
--- components/content/docs/getting-started/typography/variant/TypographyHeading.vue
+++ components/content/docs/getting-started/typography/variant/TypographyHeading.vue
@@ -6,12 +6,12 @@ import { TypographyHeadingCode } from "../typographyCode";
 <template>
   <CodeHighlightWithPreview :code="TypographyHeadingCode" :active-tab="1">
     <div class="space-y-4">
-      <h1 class="text-heading-1">The quick brown fox</h1>
-      <h2 class="text-heading-2">The quick brown fox</h2>
-      <h3 class="text-heading-3">The quick brown fox</h3>
-      <h4 class="text-heading-4">The quick brown fox</h4>
-      <h5 class="text-heading-5">The quick brown fox</h5>
-      <h6 class="text-heading-6">The quick brown fox</h6>
+      <h1 class="text-heading-1 dark:text-white">The quick brown fox</h1>
+      <h2 class="text-heading-2 dark:text-white">The quick brown fox</h2>
+      <h3 class="text-heading-3 dark:text-white">The quick brown fox</h3>
+      <h4 class="text-heading-4 dark:text-white">The quick brown fox</h4>
+      <h5 class="text-heading-5 dark:text-white">The quick brown fox</h5>
+      <h6 class="text-heading-6 dark:text-white">The quick brown fox</h6>
     </div>
   </CodeHighlightWithPreview>
 </template>
