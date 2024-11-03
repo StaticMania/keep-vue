c24b05c98586125dab968e4f7bf3b6b3cf3895ab
moshiur01 <mr.moshiur314@gmail.com>
2024-11-03 12:22:11 +0600

    Updated: typograpy heading display issue and format code base.

--- components/content/docs/getting-started/typography/variant/TypographyHeading.vue
+++ components/content/docs/getting-started/typography/variant/TypographyHeading.vue
@@ -7,7 +7,7 @@ import { TypographyHeadingCode } from "../typographyCode";
   <CodeHighlightWithPreview :code="TypographyHeadingCode" :active-tab="1">
     <div class="space-y-4">
       <h1 class="text-heading-1 dark:text-white">The quick brown fox</h1>
-      <h2 class="text-heading-2 dark:text-white">The quick brown fox</h2>
+      <p class="text-heading-2 dark:text-white">The quick brown fox</p>
       <h3 class="text-heading-3 dark:text-white">The quick brown fox</h3>
       <h4 class="text-heading-4 dark:text-white">The quick brown fox</h4>
       <h5 class="text-heading-5 dark:text-white">The quick brown fox</h5>
