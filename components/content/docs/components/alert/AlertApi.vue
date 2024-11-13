88f75ecba205b4c3593cd2b740909204f8d77505
moshiur01 <mr.moshiur314@gmail.com>
2024-11-13 17:10:08 +0600

    Fixed: Same name warning in Accordion and Alert component Api file.

--- components/content/docs/components/alert/AlertApi.vue
+++ components/content/docs/components/alert/AlertApi.vue
@@ -1,8 +1,30 @@
 <script lang="ts" setup>
 import ComponentApiTable from "~/components/content/ComponentApiTable.vue";
-import { alertApiData } from "./alertApi";
+const alertApiData = [
+  {
+    id: 1,
+    propsName: "color",
+    propsDescription: "The color variant of the alert.",
+    propsType: ["primary", "secondary", "success", "warning", "error"],
+    default: "primary",
+  },
+  {
+    id: 2,
+    propsName: "dismiss",
+    propsDescription: "Enables or disables the dismiss icon.",
+    propsType: "boolean",
+    default: "false",
+  },
+  {
+    id: 3,
+    propsName: "withBg",
+    propsDescription: "Enables background color.",
+    propsType: "boolean",
+    default: "false",
+  },
+];
 </script>
 
 <template>
-  <ComponentApiTable :data="alertApiData"></ComponentApiTable>
+  <ComponentApiTable :data="alertApiData" />
 </template>
