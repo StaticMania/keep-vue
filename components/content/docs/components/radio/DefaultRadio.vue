2169fa8a95d08795733bfcd32c9402229f8079f3
moshiur01 <mr.moshiur314@gmail.com>
2024-12-04 19:16:41 +0600

    Updated: Radio compononent variant code for documentation.

--- components/content/docs/components/radio/DefaultRadio.vue
+++ components/content/docs/components/radio/DefaultRadio.vue
@@ -7,22 +7,24 @@ const picked = ref("bangladesh");
 
 <template>
   <CodeHighlightWithPreview :code="defaultRadioCode">
-    <form class="flex flex-col gap-2">
-      <legend class="mb-1 text-body-3 text-metal-600 dark:text-metal-300">
-        Choose your favorite country
-      </legend>
-      <fieldset class="flex items-center gap-2">
-        <Radio id="pk" v-model="picked" name="country" value="pakistan" />
-        <Label for="pk">Pakistan</Label>
-      </fieldset>
-      <fieldset class="flex items-center gap-2">
-        <Radio id="bd" v-model="picked" name="country" value="bangladesh" />
-        <Label for="bd">Bangladesh</Label>
-      </fieldset>
-      <fieldset class="flex items-center gap-2">
-        <Radio id="india" v-model="picked" name="country" value="india" />
-        <Label for="india">India</Label>
-      </fieldset>
-    </form>
+    <div class="flex items-center justify-center">
+      <form class="flex flex-col gap-2">
+        <legend class="mb-1 text-body-3 text-metal-600 dark:text-metal-300">
+          Choose your favorite country
+        </legend>
+        <fieldset class="flex items-center gap-2">
+          <Radio id="pk" v-model="picked" name="country" value="pakistan" />
+          <Label for="pk">Pakistan</Label>
+        </fieldset>
+        <fieldset class="flex items-center gap-2">
+          <Radio id="bd" v-model="picked" name="country" value="bangladesh" />
+          <Label for="bd">Bangladesh</Label>
+        </fieldset>
+        <fieldset class="flex items-center gap-2">
+          <Radio id="india" v-model="picked" name="country" value="india" />
+          <Label for="india">India</Label>
+        </fieldset>
+      </form>
+    </div>
   </CodeHighlightWithPreview>
 </template>
