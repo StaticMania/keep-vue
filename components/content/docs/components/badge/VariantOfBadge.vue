083f74a288164c64c7ea18d7ddd332f9d3d4ecf6
moshiur01 <mr.moshiur314@gmail.com>
2024-12-04 16:44:20 +0600

    Updated: Badge compononent variant code for documentation.

--- components/content/docs/components/badge/VariantOfBadge.vue
+++ components/content/docs/components/badge/VariantOfBadge.vue
@@ -6,21 +6,21 @@ import { variantOfBadgeCode } from "./badgeCode";
 <template>
   <CodeHighlightWithPreview :code="variantOfBadgeCode">
     <div class="space-y-4 p-2">
-      <div class="space-x-3">
+      <div class="flex items-center justify-center gap-4">
         <Badge variant="base" color="primary">Primary</Badge>
         <Badge variant="base" color="secondary">Secondary</Badge>
         <Badge variant="base" color="success">Success</Badge>
         <Badge variant="base" color="warning">Warning</Badge>
         <Badge variant="base" color="error">Error</Badge>
       </div>
-      <div class="space-x-3">
+      <div class="flex items-center justify-center gap-4">
         <Badge variant="border" color="primary">Primary</Badge>
         <Badge variant="border" color="secondary">Secondary</Badge>
         <Badge variant="border" color="success">Success</Badge>
         <Badge variant="border" color="warning">Warning</Badge>
         <Badge variant="border" color="error">Error</Badge>
       </div>
-      <div class="space-x-3">
+      <div class="flex items-center justify-center gap-4">
         <Badge variant="background" color="primary">Primary</Badge>
         <Badge variant="background" color="secondary">Secondary</Badge>
         <Badge variant="background" color="success">Success</Badge>
