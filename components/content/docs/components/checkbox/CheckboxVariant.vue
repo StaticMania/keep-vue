7fa42e5cc03393c3c2f262a26740cbfdad77180a
moshiur01 <mr.moshiur314@gmail.com>
2024-12-04 17:22:43 +0600

    Updated: Checkbox compononent variant code for documentation.

--- components/content/docs/components/checkbox/CheckboxVariant.vue
+++ components/content/docs/components/checkbox/CheckboxVariant.vue
@@ -7,26 +7,31 @@ const statusThree = ref(false);
 </script>
 <template>
   <CodeHighlightWithPreview :code="CheckboxVariantCode">
-    <div class="space-y-3 p-3">
-      <p class="text-body-3 font-normal dark:text-metal-300">
-        Checkbox Variant
-      </p>
-      <fieldset class="flex items-center gap-2">
-        <Checkbox id="default" v-model:checked="statusOne" />
-        <Label for="default">Default</Label>
-      </fieldset>
-      <fieldset class="flex items-center gap-2">
-        <Checkbox
-          id="rounded"
-          v-model:checked="statusTwo"
-          variant="rounded"
-          default-checked />
-        <Label for="rounded">Rounded</Label>
-      </fieldset>
-      <fieldset class="flex items-center gap-2">
-        <Checkbox id="circle" v-model:checked="statusThree" variant="circle" />
-        <Label for="circle">Circle</Label>
-      </fieldset>
+    <div class="flex items-center justify-center">
+      <div class="space-y-3 p-3">
+        <p class="text-body-3 font-normal dark:text-metal-300">
+          Checkbox Variant
+        </p>
+        <fieldset class="flex items-center gap-2">
+          <Checkbox id="default" v-model:checked="statusOne" />
+          <Label for="default">Default</Label>
+        </fieldset>
+        <fieldset class="flex items-center gap-2">
+          <Checkbox
+            id="rounded"
+            v-model:checked="statusTwo"
+            variant="rounded"
+            default-checked />
+          <Label for="rounded">Rounded</Label>
+        </fieldset>
+        <fieldset class="flex items-center gap-2">
+          <Checkbox
+            id="circle"
+            v-model:checked="statusThree"
+            variant="circle" />
+          <Label for="circle">Circle</Label>
+        </fieldset>
+      </div>
     </div>
   </CodeHighlightWithPreview>
 </template>
