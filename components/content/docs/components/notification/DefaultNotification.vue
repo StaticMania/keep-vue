748c0fdb459be0df7ffafb59f2ea5b3c83716e24
moshiur01 <mr.moshiur314@gmail.com>
2024-12-04 18:13:45 +0600

    Updated: Notification compononent variant code for documentation.

--- components/content/docs/components/notification/DefaultNotification.vue
+++ components/content/docs/components/notification/DefaultNotification.vue
@@ -15,23 +15,25 @@ import { DefaultNotificationCode } from "./notificationCode";
 
 <template>
   <CodeHighlightWithPreview :code="DefaultNotificationCode">
-    <Notification>
-      <NotificationAction as-child>
-        <Button>Open Notification</Button>
-      </NotificationAction>
-      <NotificationContent class="sm:max-w-[425px]">
-        <NotificationTitle>Update Notification Status</NotificationTitle>
-        <NotificationDescription>
-          Your document has unsaved changes. Discard or save them as a new page
-          to continue.
-        </NotificationDescription>
+    <div class="flex items-center justify-center">
+      <Notification>
+        <NotificationAction as-child>
+          <Button>Open Notification</Button>
+        </NotificationAction>
+        <NotificationContent class="sm:max-w-[425px]">
+          <NotificationTitle>Update Notification Status</NotificationTitle>
+          <NotificationDescription>
+            Your document has unsaved changes. Discard or save them as a new
+            page to continue.
+          </NotificationDescription>
 
-        <NotificationFooter>
-          <NotificationClose as-child>
-            <Button>Accept</Button>
-          </NotificationClose>
-        </NotificationFooter>
-      </NotificationContent>
-    </Notification>
+          <NotificationFooter>
+            <NotificationClose as-child>
+              <Button>Accept</Button>
+            </NotificationClose>
+          </NotificationFooter>
+        </NotificationContent>
+      </Notification>
+    </div>
   </CodeHighlightWithPreview>
 </template>
