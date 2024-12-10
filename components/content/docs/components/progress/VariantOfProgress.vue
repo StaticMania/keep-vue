cd4ebe7e81d58421767d3b202d933a7690e20875
moshiur01 <mr.moshiur314@gmail.com>
2024-12-10 18:56:05 +0600

    Updated: Circle progress text and progress variant.

--- components/content/docs/components/progress/VariantOfProgress.vue
+++ components/content/docs/components/progress/VariantOfProgress.vue
@@ -1,26 +1,16 @@
 <script lang="ts" setup>
 import CodeHighlightWithPreview from "~/components/content/CodeHighlightWithPreview.vue";
-import {
-  CircleProgress,
-  CircleProgressLine,
-  CircleProgressText,
-  LineProgress,
-  LineProgressBar,
-  LineProgressText,
-} from "~/src";
+import { CircleProgress, CircleProgressLine, CircleProgressText } from "~/src";
 import { VariantOfProgressCode } from "./progressCode";
 </script>
 <template>
   <CodeHighlightWithPreview :code="VariantOfProgressCode">
-    <div class="mx-auto flex max-w-lg flex-col gap-5">
+    <div
+      class="mx-auto flex max-w-lg flex-col items-center justify-center gap-5">
       <CircleProgress :progress="55">
         <CircleProgressLine />
         <CircleProgressText />
       </CircleProgress>
-      <LineProgress :progress="55">
-        <LineProgressBar />
-        <LineProgressText />
-      </LineProgress>
     </div>
   </CodeHighlightWithPreview>
 </template>
