f50451883bdc96dcedb39e91774c00c660bee691
moshiur01 <mr.moshiur314@gmail.com>
2024-12-11 18:45:05 +0600

    Updated: Input Component.

--- src/components/Input/inputTheme.ts
+++ src/components/Input/inputTheme.ts
@@ -1,10 +1,12 @@
+import { keepTw } from "~/src/utils/keepTw";
+
 interface KeepInputTheme {
   input: string;
   icon: string;
 }
 
-export const inputTheme: KeepInputTheme = {
+export const inputTheme: KeepInputTheme = keepTw({
   input:
-    "flex h-11 w-full rounded-lg border px-3 py-2 text-body-4 font-normal text-metal-900 placeholder:font-normal placeholder:text-metal-200 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-metal-200 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 bg-white dark:bg-metal-900 dark:border-metal-800 dark:focus-visible:ring-metal-900 dark:text-white dark:placeholder:text-metal-300",
+    "flex h-11 w-full rounded-lg border px-3 py-2 text-body-4 font-normal text-metal-900 placeholder:font-normal placeholder:text-metal-200 focus-visible:outline-none focus-visible:ring-2 focus-visible:border-metal-600  focus-visible:ring-metal-200 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 bg-white dark:bg-metal-900 dark:border-metal-500 dark:focus-visible:border-metal-100 dark:focus-visible:ring-metal-700 dark:focus-visible:ring-offset-metal-900 dark:text-white dark:placeholder:text-metal-300",
   icon: "pointer-events-none absolute inset-y-0 start-0 flex items-center ps-4 peer-disabled:pointer-events-none peer-disabled:opacity-50",
-};
+});
