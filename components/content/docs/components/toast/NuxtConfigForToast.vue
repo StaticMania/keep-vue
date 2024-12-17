91446b412845542e687c647a692b12d0095e3574
moshiur01 <mr.moshiur314@gmail.com>
2024-12-17 20:11:21 +0600

    Updated: Toast documention.

---
+++ components/content/docs/components/toast/NuxtConfigForToast.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+import CodeHighlight from "~/components/content/CodeHighlight.vue";
+import { nuxtConfigForToast } from "./toastCode";
+</script>
+
+<template>
+  <CodeHighlight :code="nuxtConfigForToast" />
+</template>
