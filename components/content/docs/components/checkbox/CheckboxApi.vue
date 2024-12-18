e05f9e7e6ffe853b5ce9468d403eac9f6dcddadb
moshiur01 <mr.moshiur314@gmail.com>
2024-12-18 20:08:36 +0600

    Updated: Checkbox component, its variants and api.

--- components/content/docs/components/checkbox/CheckboxApi.vue
+++ components/content/docs/components/checkbox/CheckboxApi.vue
@@ -10,13 +10,6 @@ const CheckBoxApiData = [
     default: "default",
   },
   {
-    id: 2,
-    propsName: "default-checked",
-    propsType: "boolean",
-    propsDescription: "Checked state of the checkbox",
-    default: "false",
-  },
-  {
     id: 3,
     propsName: "checked",
     propsType: "v-model",
--- components/content/docs/components/checkbox/checkboxCode.ts
+++ components/content/docs/components/checkbox/checkboxCode.ts
@@ -3,31 +3,40 @@ const defaultCheckboxCode = {
 import { Checkbox, Label } from 'keep-vue'
 import { ref } from 'vue'
 
-const statusOne = ref(false)
-const statusTwo = ref(false)
+const statusOne = ref(true);
+const statusTwo = ref(false);
 console.log(statusOne.value, statusTwo.value)
 </script>
 
 <template>
   <div class="space-y-3 p-3">
-    <p class="text-body-3 font-normal dark:text-metal-300">Keep Design System License</p>
-    <fieldset class="flex items-center gap-2">
-      <Checkbox id="single" v-model:checked="statusOne" default-checked />
-      <Label for="single">Single License</Label>
-    </fieldset>
-    <fieldset class="flex items-center gap-2">
-      <Checkbox id="team" v-model:checked="statusTwo" />
-      <Label for="team">Team License</Label>
-    </fieldset>
-  </div>
+        <p class="text-body-3 font-normal dark:text-metal-300">
+          Keep Design System License
+        </p>
+        <fieldset class="flex items-center gap-2">
+          <Checkbox
+            id="single"
+            :checked="statusOne"
+            @update:checked="(value) => (statusOne = value)" />
+          <Label for="single">Single License</Label>
+        </fieldset>
+        <fieldset class="flex items-center gap-2">
+          <Checkbox
+            id="team"
+            :checked="statusTwo"
+            @update:checked="(value) => (statusTwo = value)" />
+          <Label for="team">Team License</Label>
+        </fieldset>
+      </div>
 </template>`,
 };
 
 const CheckboxVariantCode = {
   "CheckboxComponent.vue": `<script setup>
 import { Checkbox, Label } from "keep-vue";
+
 const statusOne = ref(false);
-const statusTwo = ref(false);
+const statusTwo = ref(true);
 const statusThree = ref(false);
 
 console.log(statusOne.value, statusTwo.value, statusThree.value);
@@ -35,25 +44,34 @@ console.log(statusOne.value, statusTwo.value, statusThree.value);
 </script>
 
 <template>
-  <div class="space-y-3 p-3">
-    <p class="text-body-3 font-normal dark:text-metal-300">Checkbox Variant</p>
-    <fieldset class="flex items-center gap-2">
-      <Checkbox id="default" v-model:checked="statusOne" />
-      <Label for="default">Default</Label>
-    </fieldset>
-    <fieldset class="flex items-center gap-2">
-      <Checkbox
-        id="rounded"
-        v-model:checked="statusTwo"
-        variant="rounded"
-        default-checked />
-      <Label for="rounded">Rounded</Label>
-    </fieldset>
-    <fieldset class="flex items-center gap-2">
-      <Checkbox id="circle" v-model:checked="statusThree" variant="circle" />
-      <Label for="circle">Circle</Label>
-    </fieldset>
-  </div>
+ <div class="space-y-3 p-3">
+        <p class="text-body-3 font-normal dark:text-metal-300">
+          Checkbox Variant
+        </p>
+        <fieldset class="flex items-center gap-2">
+          <Checkbox
+            id="default"
+            :checked="statusOne"
+            @update:checked="(value) => (statusOne = value)" />
+          <Label for="default">Default</Label>
+        </fieldset>
+        <fieldset class="flex items-center gap-2">
+          <Checkbox
+            id="rounded"
+            :checked="statusTwo"
+            variant="rounded"
+            @update:checked="(value) => (statusTwo = value)" />
+          <Label for="rounded">Rounded</Label>
+        </fieldset>
+        <fieldset class="flex items-center gap-2">
+          <Checkbox
+            id="circle"
+            :checked="statusThree"
+            variant="circle"
+            @update:checked="(value) => (statusThree = value)" />
+          <Label for="circle">Circle</Label>
+        </fieldset>
+      </div>
 </template>
 `,
 };
