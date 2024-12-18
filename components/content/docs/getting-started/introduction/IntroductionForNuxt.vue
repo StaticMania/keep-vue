15db7bb0128536f42c991f900f7f00c16c727fc6
moshiur01 <mr.moshiur314@gmail.com>
2024-12-18 13:14:59 +0600

    Updated: Nuxt instalation process.

--- components/content/docs/getting-started/introduction/IntroductionForNuxt.vue
+++ components/content/docs/getting-started/introduction/IntroductionForNuxt.vue
@@ -4,6 +4,8 @@ import {
   addTwCssForNuxt,
   ImportKeepNuxt,
   installKeepVueForNuxt,
+  installNuxtColorMode,
+  nuxtColorModeModuleAdd,
   NuxtInstall,
   phIconNuxt,
   twConfigForNuxt,
@@ -57,11 +59,38 @@ import {
         </h1>
         <p class="text-body-4 font-normal text-metal-600 dark:text-metal-300">
           The Phosphor icon library is optional. If you prefer not to use
-          Phosphor icons, feel free to install any other icon library of your
-          choice.
+          Phosphor icons, you can install any other icon library of your choice.
+          For example, we have used the @phosphor-icons/vue package. If you
+          decide to use this library, you must follow the Nuxt convention for
+          Phosphor icons.
         </p>
         <div class="!my-4">
           <CodeHighlight :code="installKeepVueForNuxt" />
+        </div>
+      </TimelineContent>
+    </TimelineItem>
+
+    <TimelineItem class="max-w-full">
+      <TimelinePoint />
+      <TimelineContent>
+        <p
+          class="text-body-5 font-normal leading-[1.4] text-metal-400 dark:text-metal-300">
+          Step 4
+        </p>
+        <h1 class="text-body-3 font-medium text-metal-900 dark:text-white">
+          Install Nuxt color mode to switch dark and light mode.
+        </h1>
+
+        <div class="!my-4">
+          <CodeHighlight :code="installNuxtColorMode" />
+        </div>
+        <div class="!my-4">
+          <p
+            class="text-body-4 font-normal leading-[1.4] text-metal-500 dark:text-metal-300">
+            Then, add @nuxtjs/color-mode to the modules section of your
+            nuxt.config.ts
+          </p>
+          <CodeHighlight :code="nuxtColorModeModuleAdd" />
         </div>
       </TimelineContent>
     </TimelineItem>
